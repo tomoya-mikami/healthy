51bfef8ed323f20d8d38621871f685a20c44e215
Tomoya Mikami <trymorecc@gmail.com>
2021-11-21 00:50:02 +0900

    fix requre

--- src/components/fugafuga.tsx
+++ src/components/fugafuga.tsx
@@ -4,13 +4,25 @@ import { Accelerometer, ThreeAxisMeasurement } from 'expo-sensors';
 import { post } from "../utils/request";
 
 const UPDATE_MS = 100;
-const DATA_QUE_SIZE = 100;
+const DATA_QUE_SIZE = 30;
 const TEST_URL = "http://1c77-180-39-77-67.ngrok.io/mochiage/1";
 
 interface MLRequest {
-  dig: ThreeAxisMeasurement,
-  acc: ThreeAxisMeasurement
+  digx: number,
+  digy: number,
+  digz: number
+  //acc: ThreeAxisMeasurement
 }
+
+const diffMeasurement = (
+  before: ThreeAxisMeasurement,
+  after: ThreeAxisMeasurement
+): number => {
+  const beforeValue = round(before.x);
+  const afterValue = round(after.x);
+
+  return afterValue - beforeValue;
+};
 
 const round = (n: number | null) => {
   if (!n) {
@@ -22,6 +34,9 @@ const round = (n: number | null) => {
 
 export const Fugafuga: FC = (props: any) => {
   const [data, setData] = React.useState<ThreeAxisMeasurement>({ x: 0, y: 0, z: 0 });
+  const [lastThreeAxisMeasurement, setLastThreeAxisMeasurement] = React.useState<
+    ThreeAxisMeasurement
+  >({ x: 0, y: 0, z: 0 });
   const [dataQue, setDataQue] = React.useState<MLRequest[]>([]);
   const [subscription, setSubscription] = React.useState<any | undefined>(
     undefined
@@ -61,13 +76,13 @@ export const Fugafuga: FC = (props: any) => {
   }, [dataQue]);
   React.useEffect(() => {
     (async () => {
-      const dig = {
-        x: round(data.x),
-        y: round(data.y),
-        z: round(data.z)
+      const accRequest: MLRequest = {
+        digx: round(data.x),
+        digy: round(data.y),
+        digz: round(data.z)
       };
       const newDataQue: MLRequest[] = JSON.parse(JSON.stringify(dataQue));
-      newDataQue.push({dig: dig, acc: {x:0, y:0, z:0}});
+      newDataQue.push(accRequest);
       setDataQue(newDataQue);
     })();
   }, [data]);
